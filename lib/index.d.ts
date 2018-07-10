60c3c5270d252daa9892fa16a967647ac7a41af2
Steffan Sluis <steffansluis@gmail.com>
2018-07-10 14:43:59 +0200

    Add VideoAnnotation and startDuration

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 import { Operation } from 'memux';
 import { Doc } from 'feedbackfruits-knowledge-engine';
 export declare type SendFn = (operation: Operation<Doc>) => Promise<void>;
-export default function init({name}: {
+export default function init({ name }: {
     name: any;
 }): Promise<void>;
